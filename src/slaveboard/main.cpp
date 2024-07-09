06241c754229e1e09198255bf1444d17091ac0b7
Joshua Riefman <joshuariefman@gmail.com>
2024-07-09 13:27:52 -0700

    Add square movement

--- src/slaveboard/main.cpp
+++ src/slaveboard/main.cpp
@@ -7,38 +7,36 @@
 #include <string.h>
 #include <FreeRTOS.h>
 #include <task.h>
-
-#define MOTOR_1_FORWARD_PIN PA1
-#define MOTOR_1_REVERSE_PIN PA0
-#define MOTOR_2_FORWARD_PIN PA3
-#define MOTOR_2_REVERSE_PIN PA2
-#define MOTOR_3_FORWARD_PIN PA7
-#define MOTOR_3_REVERSE_PIN PA6
-#define MOTOR_4_FORWARD_PIN PB1
-#define MOTOR_4_REVERSE_PIN PB0
-
 #include <tape/task_poll_reflectance.h>
 #include <tape/reflectance_polling_config.h>
-ReflectancePollingConfig config;
-CircularBuffer<int, BUFFER_SIZE> leftBuffer;
-CircularBuffer<int, BUFFER_SIZE> rightBuffer;
+
+#define MOTOR_BACK_RIGHT_FORWARD PB1
+#define MOTOR_BACK_RIGHT_REVERSE PB0
+#define MOTOR_BACK_LEFT_FORWARD PA7
+#define MOTOR_BACK_LEFT_REVERSE PA6
+#define MOTOR_FRONT_LEFT_FORWARD PA1
+#define MOTOR_FRONT_LEFT_REVERSE PA0
+#define MOTOR_FRONT_RIGHT_FORWARD PA3
+#define MOTOR_FRONT_RIGHT_REVERSE PA2
+
+#define SPEED 16000
+#define TIME_DELAY_MOTOR 500
 
 #define PRIORITY_REFLECTANCE_POLLING 3
 #define PRIORITY_MOTOR_ADJUSTMENT 1
 
-
-
-
-
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET  -1 // This display does not have a reset pin accessible
-Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-RobotMotor motor_1;
-RobotMotor motor_2;
-RobotMotor motor_3;
-RobotMotor motor_4;
+Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+RobotMotor motor_front_left;
+RobotMotor motor_front_right;
+RobotMotor motor_back_left;
+RobotMotor motor_back_right;
+ReflectancePollingConfig config;
+CircularBuffer<int, BUFFER_SIZE> leftBuffer;
+CircularBuffer<int, BUFFER_SIZE> rightBuffer;
 
 
 void setup() {
@@ -73,10 +71,11 @@ void setup() {
     Serial.println("Poll was not created successfully!");
   }
 
-  motor_1 = RobotMotor(MOTOR_1_FORWARD_PIN, MOTOR_1_REVERSE_PIN);
-  motor_2 = RobotMotor(MOTOR_2_FORWARD_PIN, MOTOR_2_REVERSE_PIN);
-  motor_3 = RobotMotor(MOTOR_3_FORWARD_PIN, MOTOR_3_REVERSE_PIN);
-  motor_4 = RobotMotor(MOTOR_4_FORWARD_PIN, MOTOR_4_REVERSE_PIN);
+  motor_front_left = RobotMotor(MOTOR_FRONT_LEFT_FORWARD, MOTOR_FRONT_LEFT_REVERSE);
+  motor_front_right = RobotMotor(MOTOR_FRONT_RIGHT_FORWARD, MOTOR_FRONT_RIGHT_REVERSE);
+  motor_back_left = RobotMotor(MOTOR_BACK_LEFT_FORWARD, MOTOR_BACK_LEFT_REVERSE);
+  motor_back_right = RobotMotor(MOTOR_BACK_RIGHT_FORWARD, MOTOR_BACK_RIGHT_REVERSE);
+
 
   // size_t freeHeap = xPortGetFreeHeapSize();
   // Serial.println("Free Heap: " + String(freeHeap));
@@ -86,22 +85,47 @@ void setup() {
 
 
 void loop() {
-  // motor_1.set_drive(0, forward);
-  // motor_2.set_drive(0, forward);
-  // motor_3.set_drive(0, forward);
-  // motor_4.set_drive(0, forward);
+  motor_front_left.set_drive(SPEED, forward);
+  motor_front_right.set_drive(SPEED, forward);
+  motor_back_left.set_drive(SPEED, forward);
+  motor_back_right.set_drive(SPEED, forward);
+  delay(TIME_DELAY_MOTOR);
+  motor_front_left.stop();
+  motor_front_right.stop();
+  motor_back_left.stop();
+  motor_back_right.stop();
+  delay(1000);
 
-  // delay(2000);
+  motor_front_left.set_drive(SPEED, forward);
+  motor_front_right.set_drive(SPEED, reverse);
+  motor_back_left.set_drive(SPEED, reverse);
+  motor_back_right.set_drive(SPEED, forward);
+  delay(TIME_DELAY_MOTOR * 2);
+  motor_front_left.stop();
+  motor_front_right.stop();
+  motor_back_left.stop();
+  motor_back_right.stop();
+  delay(1000);
 
-  // motor_1.set_drive(32000, forward);
-  // motor_2.set_drive(32000, forward);
-  // motor_3.set_drive(32000, forward);
-  // motor_4.set_drive(32000, forward);
+  motor_front_left.set_drive(SPEED, reverse);
+  motor_front_right.set_drive(SPEED, reverse);
+  motor_back_left.set_drive(SPEED, reverse);
+  motor_back_right.set_drive(SPEED, reverse);
+  delay(TIME_DELAY_MOTOR);
+  motor_front_left.stop();
+  motor_front_right.stop();
+  motor_back_left.stop();
+  motor_back_right.stop();
+  delay(1000);
 
-  // delay(250);
-  // Print the last 5 values in each buffer
-
-
-  // Add a delay to prevent flooding the serial output
-  delay(1000); // Print every second
-}
+  motor_front_left.set_drive(SPEED, reverse);
+  motor_front_right.set_drive(SPEED, forward);
+  motor_back_left.set_drive(SPEED, forward);
+  motor_back_right.set_drive(SPEED, reverse);
+  delay(TIME_DELAY_MOTOR * 2);
+  motor_front_left.stop();
+  motor_front_right.stop();
+  motor_back_left.stop();
+  motor_back_right.stop();
+  delay(1000);
+  }
