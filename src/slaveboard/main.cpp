1c74e7abd0fd23222eddceeb2f51ee5cb7cae97d
Joshua Riefman <joshuariefman@gmail.com>
2024-07-12 16:21:11 -0700

    UNTESTED ON ROBOT: Reformatting

--- src/slaveboard/main.cpp
+++ src/slaveboard/main.cpp
@@ -34,94 +34,94 @@ QueueHandle_t xSharedQueue = xQueueCreate(10, sizeof(Message));
 
 void TaskMaster(void *pvParameters);
 
-void setup()
-{
-  Serial.begin(9600);
+void setup() {
+    Serial.begin(9600);
 
-  checkResetCause();
+    checkResetCause();
 
-  motor_front_left = RobotMotor(MOTOR_FRONT_LEFT_FORWARD, MOTOR_FRONT_LEFT_REVERSE);
-  motor_front_right = RobotMotor(MOTOR_FRONT_RIGHT_FORWARD, MOTOR_FRONT_RIGHT_REVERSE);
-  motor_back_left = RobotMotor(MOTOR_BACK_LEFT_FORWARD, MOTOR_BACK_LEFT_REVERSE);
-  motor_back_right = RobotMotor(MOTOR_BACK_RIGHT_FORWARD, MOTOR_BACK_RIGHT_REVERSE);
+    motor_front_left = RobotMotor(MOTOR_FRONT_LEFT_FORWARD, MOTOR_FRONT_LEFT_REVERSE);
+    motor_front_right = RobotMotor(MOTOR_FRONT_RIGHT_FORWARD, MOTOR_FRONT_RIGHT_REVERSE);
+    motor_back_left = RobotMotor(MOTOR_BACK_LEFT_FORWARD, MOTOR_BACK_LEFT_REVERSE);
+    motor_back_right = RobotMotor(MOTOR_BACK_RIGHT_FORWARD, MOTOR_BACK_RIGHT_REVERSE);
 
-  robotMotors = { &motor_front_right, &motor_front_left, &motor_back_right, &motor_back_left};
+    robotMotors = { &motor_front_right, &motor_front_left, &motor_back_right, &motor_back_left};
 
-  config_reflectance = { &leftBuffer, &rightBuffer };
-  config_following = { &robotMotors, &config_reflectance };
-  config_rotate = { &config_following, &xSharedQueue};
+    config_reflectance = { &leftBuffer, &rightBuffer };
+    config_following = { &robotMotors, &config_reflectance };
+    config_rotate = { &config_following, &xSharedQueue};
 
-  BaseType_t xReturnedReflectance = xTaskCreate(TaskPollReflectance, "ReflectancePolling", 72, &config_reflectance, PRIORITY_REFLECTANCE_POLLING, &xReflectanceHandle);
-  BaseType_t xReturnedFollowing = xTaskCreate(TaskFollowTape, "Tape Following", 64, &config_following, PRIORITY_FOLLOW_TAPE, &xHandleFollowing);
-  BaseType_t xReturnedRotate = xTaskCreate(TaskRotate, "TapeRotate", 64, &config_rotate, PRIORITY_FOLLOW_TAPE, &xHandleRotating);
-  BaseType_t xMasterReturned = xTaskCreate(TaskMaster, "MasterTask", 64, NULL, 1, &xMasterHandle);
+    // check if reflectance polling task was created
+    if (xTaskCreate(TaskPollReflectance, "ReflectancePolling", 72, &config_reflectance, PRIORITY_REFLECTANCE_POLLING, &xReflectanceHandle) == pdPASS) {
+        Serial.println("Reflectance polling task was created successfully."); 
+    } else {
+        Serial.println("Reflectance polling task was not created successfully!");
+    }
 
-  vTaskSuspend(xHandleRotating);
-  vTaskSuspend(xHandleFollowing);
+    // check if tape following task was created
+    if (xTaskCreate(TaskFollowTape, "Tape Following", 64, &config_following, PRIORITY_FOLLOW_TAPE, &xHandleFollowing) == pdPASS) {
+        Serial.println("Tape following task was created successfully.");
+    } else {
+        Serial.println("Tape following task was not created successfully!");
+    }
 
-  // check if reflectance polling task was created
-  if (xReturnedReflectance == pdPASS) {
-    Serial.println("Reflectance polling task was created successfully."); 
-  } else {
-    Serial.println("Reflectance polling task was not created successfully!");
-  }
+    // check if tape following task was created
+    if (xTaskCreate(TaskRotate, "TapeRotate", 64, &config_rotate, PRIORITY_FOLLOW_TAPE, &xHandleRotating) == pdPASS) {
+        Serial.println("Rotate task was created successfully.");
+    } else {
+        Serial.println("Rotate task was not created successfully!");
+    }
 
-  // check if tape following task was created
-  if (xReturnedFollowing == pdPASS) {
-    Serial.println("Tape following task was created successfully.");
-  } else {
-    Serial.println("Tape following task was not created successfully!");
-  }
+    // check if tape following task was created
+    if (xTaskCreate(TaskMaster, "MasterTask", 64, NULL, 1, &xMasterHandle) == pdPASS) {
+        Serial.println("Master task was created successfully.");
+    } else {
+        Serial.println("Master task was not created successfully!");
+    }
 
-  // check if tape following task was created
-  if (xReturnedRotate == pdPASS) {
-    Serial.println("Rotate task was created successfully.");
-  } else {
-    Serial.println("Rotate task was not created successfully!");
-  }
+    vTaskSuspend(xHandleRotating);
+    vTaskSuspend(xHandleFollowing);
 
-  // check if tape following task was created
-  if (xMasterReturned == pdPASS) {
-    Serial.println("Master task was created successfully.");
-  } else {
-    Serial.println("Master task was not created successfully!");
-  }
+    size_t freeHeap = xPortGetFreeHeapSize();
+    Serial.println("Free Heap: " + String(freeHeap));
 
-  size_t freeHeap = xPortGetFreeHeapSize();
-  Serial.println("Free Heap: " + String(freeHeap));
-
-  vTaskStartScheduler();
+    vTaskStartScheduler();
 }
 
 void loop()
 {
-  monitorStackUsage(&xHandleRotating, &xReflectanceHandle, &xHandleFollowing, &xMasterHandle); // Monitor stack usage periodically
-  delay(500);
+    monitorStackUsage(&xHandleRotating, &xReflectanceHandle, &xHandleFollowing, &xMasterHandle); // Monitor stack usage periodically
+    delay(500);
 }
 
 void TaskMaster(void *pvParameters)
 {
-  Message receivedMessage;
-  while (1)
-  {
-    // rotate until completion
-    vTaskSuspend(xHandleFollowing);
-    Serial.println("chill for a sec!");
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    Serial.println("Rotating!");
-    vTaskResume(xHandleRotating);
+    log_status("Beginning master task...");
+    Message receivedMessage;
+    while (1) {
+        log_status("State 0");
+        // rotate until completion
+        vTaskSuspend(xHandleFollowing);
 
-    // // wait until a message is recieved on the shared queue
-    // // this saves the value of the message into "receivedMessage"
-    // this operation is blocking since we passed "portMAX_DELAY"
-    if (xQueueReceive(xSharedQueue, &receivedMessage, portMAX_DELAY) == pdPASS) {
-      if(receivedMessage == ROTATION_DONE) {
-        Serial.println("Tape Following!");
-        // tape follow for 1 second
-        vTaskResume(xHandleFollowing);
-        vTaskSuspend(xHandleRotating);
-        vTaskDelay(pdMS_TO_TICKS(1500));
-      }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        vTaskResume(xHandleRotating);
+
+        // // wait until a message is recieved on the shared queue
+        // // this saves the value of the message into "receivedMessage"
+        // this operation is blocking since we passed "portMAX_DELAY"
+        log_status("State 1");
+
+        if (xQueueReceive(xSharedQueue, &receivedMessage, portMAX_DELAY) == pdPASS) {
+            if(receivedMessage == ROTATION_DONE) {
+                log_status("State 2");
+                
+                // tape follow for 1 second
+                vTaskResume(xHandleFollowing);
+                vTaskSuspend(xHandleRotating);
+                vTaskDelay(pdMS_TO_TICKS(1500));
+
+                log_status("State 3");
+            }
+        }
     }
-  }
 }
