6422c097e78e27c3af6cd410d976466408cd2647
Dekempsy4 <felixtoft09@gmail.com>
2024-07-18 23:56:37 -0700

    adding cook station enum

---
+++ include/motion/stations.h
@@ -0,0 +1,21 @@
+// numbered left to right
+enum CookStationsServer {
+    TOMATO = 1,
+    PATTIES = 2, 
+    CUTTING = 3,
+    BUNS = 4,
+    COOKTOP = 5,
+    POTATOES = 6,
+    PLATES = 7,
+};
+
+enum CookStationsChef {
+    // Tomato and cheese are lined up
+    TOMATO = 1,
+    CHEESE = 1,
+    CUTTING = 2,
+    SERVING = 3,
+    COOKTOP = 4,
+    PLATES = 5,
+    LETTUCE = 6,
+};
