fc4140fda170043e803acce51d1200ff3976e629
Joshua Riefman <joshuariefman@gmail.com>
2024-07-19 15:52:45 -0700

    Rename robots

---
+++ include/chef_robot/constants.h
@@ -0,0 +1,12 @@
+#ifndef CHEF_ROBOT_CONSTANTS_H
+#define CHEF_ROBOT_CONSTANTS_H
+
+// Pin Definitions
+#define STEPPER_CONTROL_PIN     7
+#define STEPPER_DIRECTION_PIN   5
+
+#define RX_PIN              21
+#define TX_PIN              22
+
+
+#endif // CHEF_ROBOT_CONSTANTS_H
---
+++ include/serving_robot/constants.h
@@ -0,0 +1,12 @@
+#ifndef SERVING_ROBOT_CONSTANTS_H
+#define SERVING_ROBOT_CONSTANTS_H
+
+// Pin Definitions
+#define STEPPER_DIRECTION_PIN   7
+#define STEPPER_CONTROL_PIN     5
+
+#define RX_PIN              21
+#define TX_PIN              22
+
+
+#endif // SERVING_ROBOT_CONSTANTS_H
