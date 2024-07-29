62bf437cc49d3ece3bcb9a09b7fbb08a301e90a3
Joshua Riefman <joshuariefman@gmail.com>
2024-07-28 22:37:53 -0700

    Completely redo UART

--- include/communication/decode.h
+++ include/communication/decode.h
@@ -12,12 +12,12 @@ typedef enum { ROTATION_DONE, LOST_TAPE, REACHED_POSITION } StatusMessage_t;
 /**
  * @brief Enum discretizing the different kinds of messages being passed between boards
  */
-typedef enum { NONE = 0x00, GOTO = 0x01, DO_SPIN = 0x02, COMPLETED = 0x03, ABORT = 0x04, OCCUPIED = 0x05, ACCEPTED = 0x06, READY = 0x07, COUNTER_DOCK = 0x08, TAPE_RETURN = 0x09 } CommandMessage_t;   
+typedef enum { READY = 0x00, GOTO = 0x01, DO_SPIN = 0x02, ABORT = 0x03, TAPE_RETURN = 0x04, COUNTER_DOCK = 0x05, COMPLETED = 0x06, OCCUPIED = 0x07, ACCEPTED = 0x08, ACK = 0x09, NACK = 0x0a } CommandMessage_t;   
 
-/**
- * @brief Decode a byte into a CommandMessage_t
- */
-CommandMessage_t decode_command(uint8_t message_identifier);
+// /**
+//  * @brief Decode a byte into a CommandMessage_t
+//  */
+// CommandMessage_t decode_command(uint8_t message_identifier);
 
 
 #endif // DECODE_COMMUNICATION_H
