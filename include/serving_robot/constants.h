5ac0886e5120b1054a5c4cb1c3dec26c317f61d4
Joshua Riefman <joshuariefman@gmail.com>
2024-07-30 19:27:51 -0700

    Change pins

--- include/serving_robot/constants.h
+++ include/serving_robot/constants.h
@@ -2,24 +2,23 @@
 #define SERVING_ROBOT_CONSTANTS_H
 
 // Pin Definitions
-#define STEPPER_SLEEP_PIN               15
-#define STEPPER_CONTROL_PIN             4
+#define SERVO_VERTICAL_PIN              4
+
 #define RACK_FORWARD_PIN                7
 #define RACK_REVERSE_PIN                8
 
-#define CLAW_ACTUATE_PIN_1                32
-#define CLAW_ACTUATE_PIN_2                33
-
+#define CLAW_ACTUATE_PIN_1              32
+#define CLAW_ACTUATE_PIN_2              33
 
 #define SERVO_CLAW_PIN                  13
 #define SERVO_CLAW_OPEN                 90 // open
 #define SERVO_CLAW_CLOSED               30 // closed
 
-#define SERVO_DRAW_BRIDGE_PIN           19
+#define SERVO_DRAW_BRIDGE_PIN           22
 #define SERVO_DRAW_BRIDGE_DOWN          87
 #define SERVO_DRAW_BRIDGE_UP            180
 
-#define SERVO_PLATE_PIN                 22
+#define SERVO_PLATE_PIN                 19
 #define SERVO_PLATE_OPEN                90 // open
 #define SERVO_PLATE_CLOSED              30 // closed
 
