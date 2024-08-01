7aa6992974b3cab0b64411f812a76cb4681125fb
Dekempsy4 <felixtoft09@gmail.com>
2024-08-01 13:57:09 -0700

    adding servo constants for chef robot

--- include/chef_robot/constants.h
+++ include/chef_robot/constants.h
@@ -2,11 +2,22 @@
 #define CHEF_ROBOT_CONSTANTS_H
 
 // Pin Definitions
-#define STEPPER_CONTROL_PIN     7
-#define STEPPER_DIRECTION_PIN   5
 
 #define RX_PIN              21
 #define TX_PIN              22
+
+
+#define SERVO_VERTICAL_PIN              4
+#define SERVO_VERTICAL_DOWN             0.098
+#define SERVO_VERTICAL_UP               0.022
+
+
+
+
+
+
+
+
 
 
 #endif // CHEF_ROBOT_CONSTANTS_H
