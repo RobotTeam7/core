6287f80a4bf6ee05a6911fd75aea77ec44a1d74e
Joshua Riefman <joshuariefman@gmail.com>
2024-08-05 14:34:54 -0700

    More changes II

--- include/motion/state.h
+++ include/motion/state.h
@@ -11,23 +11,21 @@ typedef enum { GOTO_STATION, SPIN, IDLE, DOCK_AT_STATION, RETURN_TO_TAPE, WALL_S
 typedef enum { DRIVE, ROTATE, STOP, TRANSLATE, ROTATE_AND_TRANSLATE } DriveState_t;
 
 typedef struct {
-    // Memory
-    int orientation;                    // Which direction is the robot facing -> 1: right, -1: left
-    int8_t last_station;               // Which station are we at (or was at before moving)
-    int8_t last_side_station;          // When hugging a counter, which station were we last at
-    int16_t pirouette_angle;
-
     // Localization
-    int8_t desired_station;
-    int8_t desired_side_station;        // this value isn't a uint because sometimes we will send pirouette commands to return to the same side
-    int direction;                      // Which direction should the robot drive in -> 1: forward, -1: reverse
-    int y_direction;                    // Which direction to translate -> 1: up, 0: on tape, -1: down                
-    int helicity;                       // Which direction should the robot rotate -> 1: counterclockwise, -1: clockwise
+    int orientation;                // Which direction is the robot facing -> 1: right, -1: left
+    int16_t pirouette_angle;
+    int8_t last_side_station;       // When hugging a counter, which station were we last at
+    int y_direction;                // Which direction are we displaced from tape -> 1: up, 0: on tape, -1: down                
     
-    // Control
-    int yaw;                            // pid-updated drive state
-    int16_t drive_speed;               // base speed in which motors should be engaged
-    float speed_modifier;               // multiplier to ALL drive speeds (should be a percentage, 0.0-1.0)
+    // Drive
+    int helicity;                   // Which direction should the robot rotate -> 1: counterclockwise, -1: clockwise
+    int direction;                  // Which direction should the robot drive in -> 1: forward, -1: reverse
+    int yaw;                        // pid-updated drive state
+    int16_t drive_speed;            // base speed in which motors should be engaged
+    float speed_modifier;           // multiplier to ALL drive speeds (should be a percentage, 0.0-1.0)
+
+    // Command
+    int8_t desired_side_station;    // this value isn't a uint because sometimes we will send pirouette commands to return to the same side
     DriveState_t drive_state;
     ActionType_t current_action;
 } State_t;
